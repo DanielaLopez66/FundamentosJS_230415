a497f829e209069d28e7791aceb1c08e39bbb3c6
Daniela Lopez <230415@utxicotepec.edu.mx>
2024-10-10 14:09:15 -0600

    Practica 7
    Nueva informacion

--- js/Ejercicio05.js
+++ js/Ejercicio05.js
@@ -1,144 +1,281 @@
-// Repaso de  Ciclo y Sentencias de Control
-
 //Estilización de los mensajes de salida
-const bg = "linear-gradient(14deg, rgba(0,250,50,10) 0%, rgba(150,250,30,1) 33%, rgba(150,250,50,1) 86%)";
+const bg = "linear-gradient(11deg, rgba(200,150,360,1) 0%, rgba(260,129,12,1) 33%, rgba(255,19,260,1) 86%)";
 const style_console = `background: ${bg}; color: white; border-radius: 6px; padding: 4px; font-size: 1.0rem; font-weight: bold`
 
-//Personalización de las Salidas a Consola
+
+//Personalización de las salidas a consola
+
 console.warn("Práctica 07: Arreglos en Java Script")
 
+console.log("%c1.- Si/Entoces ... (IF)", style_console)
 
-console.log("%c1.- Si/Entonces ... (IF)", style_console);
-//Le indica al programa que hacer o que no en vase a una prueba lógica de verdadero  o falso
-let fechaActual= new Date();
+//Le indica al programa que hacer o que no en base a una prueba lógica de verdadero o falso 
+let fechaActual=new Date();
+console.log(`Hola, la fecha de hoy es: ${fechaActual.toDateString()}`)
 
-console.log(`Hola, la fecha de hoy es: ${fechaActual.toString()}`);
+//Y si la necesitamos en formato local(?)
 
-// y si la necesitamos en formato local?
-const fechaLocalMX = fechaActual.toLocaleString('es-MX',
-    {
-        weekday: 'long',
-        year: 'numeric',
-        month:'long',
-        day: 'numeric',
-        hour: 'numeric',
-        minute: 'numeric',
-        second: 'numeric',
-        hour12: false
-    });
+const fechaLocalMX= fechaActual.toLocaleString('es-MX', {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+    hour12: false
+});
 
-console.log(`en formato local (México): ${fechaLocalMX}`);
+console.log(`en formato local (México): ${fechaLocalMX}`)
 
-// Si es antes de las doce saluda con un buenos días
+//Si es antes de las doce saluda con buenos días
 
 if(fechaActual.getHours()<12)
-console.log(`Buenos días, hoy es: ${fechaLocalMX}`);
+    console.log(`Buenos días, hoy es ${fechaLocalMX}`)
 
-// Existe un extensor de la sentencia if(Sí) que es else (en caso contrario)
+//Existe un extensor de la sentencia if(Sí) que es else (En Caso contrario)
 if(fechaActual.getMonth()<=6)
-    console.log(`Estas en la primera mitad del año.`);
+    console.log(`Estas en la primera mitad del año`)
 else
-    console.log(`Estas en la segunda mitad del año.`);
-        
+    console.log(`Estas en la segunda mitad del año.`)
 
-// Que pasa si la validación tiene varias operaciones
 
-const anio = fechaActual.getFullYear();
-let inicioPrimavera = new Date(anio, 2,21);
-let inicioVerano = new Date(anio, 5,21);
-let inicioOtonio = new Date(anio, 8,23);
-let inicioInvierno = new Date(anio, 11,21);
-
+//que pasa si la validación tiene varias operaciones 
+const anio=fechaActual.getFullYear();
+let inicioPrimavera=new Date(anio, 2, 21);
+let inicioVerano=new Date(anio, 5, 21);
+let inicioOtonio=new Date(anio, 8, 23);
+let inicioInvierno=new Date(anio, 11, 21);
+let estacion;
 let horarioVerano=false;
 
-if(fechaActual >= inicioPrimavera || fechaActual< inicioVerano)
-{
-    console.log("Estamos en PRIMAVERA...")
-    console.log("Inicia la floracion de muchas plantas ...")
-    console.log("Los días son más largos y las noches más cortas ...")
-    console.log("Muchos animales despiertan de la hibernación ...")
-    console.log()
-    estacion="Primavera"
+if(fechaActual>=inicioPrimavera && fechaActual<inicioVerano){
+    console.log(`Estamos en PRIMAVERA`);
+    console.log("Inicia la floración de muchas plantas.")
+    console.log("Los días son más largos y las noches más cortas.")
+    console.log("Muchos animales despiertan de la hibernación.")
+    estacion="Primavera";
     horarioVerano=true;
-}
-else if(fechaActual>=inicioVerano || fechaActual<inicioOtonio)
-{
-    console.log("Estamos en VERANO...")
-    console.log("En esta temporada abundan los dias soleadps y calurosos ...")
-    console.log("En esta temporada el indice de turismo vacacional sube ...")
-    console.log("Muchos gente busca realizar las actividades al aire ...")
-    estacion="Verano"
+} else if(fechaActual>=inicioVerano&&fechaActual<inicioOtonio){
+    console.log("Estamos en VERANO");
+    console.log("En esta temporada abundan los días soleados y calurosos.")
+    console.log("En esta temporada el índice de turismo vacacional sube.")
+    console.log("Mucha gente busca realizar actividades al aire.")
+    estacion="Verano";
     horarioVerano=true;
-}
-else if(fechaActual>=inicioOtonio || fechaActual<inicioInvierno)
-    {
-        console.log("Estamos en OTOÑO...")
-        console.log("Los árboles suelen cambiar de follaje")
-        console.log("Se registran temperaturas mas templadas")
-        console.log("Los animales empiezan con la recoleccion de alimento y preparan sus espacios para la hibernacion, incluso algunas aves migran. ")
-        estacion="Otoño"
-        horarioVerano=true;
-    }
-else
-{
-    console.log("Estamos en Invierno...")
-    console.log("En esta temporada los dias son más cortos y las nochesmas largas ...")
-    console.log("En algunas regiones puede nevar ...")
-    console.log("Dadas las temperaturas se recomienda abrigarse  ...")
-    console.log()
-    estacion="Invierno"
+}else if(fechaActual>=inicioOtonio&&fechaActual<inicioInvierno){
+    console.log("Estamos en OTOÑO")
+    console.log("Los árboles suelen cambiar de follaje.")
+    console.log("Se registrarán temperaturas más templadas.")
+    console.log("Los animales comienzan con la recolección de alimento y preparan sus espacios para la hibernación, incluso algunas aves migran.")
+    estacion="Otoño";
+    horarioVerano=false;
+} else {
+    console.log("Estamos en INVIERNO")
+    console.log("En esta temporada los días son más cortos y las noches más largas.")
+    console.log("En algunas regiones suele nevar.")
+    console.log("Dado las bajas temperaturas, se recomienda abrigarse.")
+    estacion="Invierno";
     horarioVerano=false;
 }
-console.log("%c2.- Operador Ternario (validacion? cumple:no_cumple) ", style_console);
-//En java Script exista una operacion simplificada que valida si una condicion se cumple o no, y que hacer en cada caso
 
-const edadPersona = 18;
-const mayoriaEdadMX = 18;
-const mayoriaEdadUS = 21;
-let evaluarMayoriaEdad = (edad)=>
-    edad>=18?"Eres mayor de edad":"No eres mayor de edad"
 
-console.log("Evaluando la mayoria de edad de una persona...")
-console.log(evaluarMayoriaEdad(edadPersona))
 
-//Sin embargo tenemos que considerar que la mayoria de edad varia en cada pais pos cuestiones legales,por lo que debemos considerar un segundo parametro en la evaluacion.
+console.log("%c2.- Operador Ternario (validación?cumple:no_cumple)", style_console);
+//En JavaScript existe una operación simplificada que valida si una condición se cumple o no, y que hacer en cada caso.
 
-evaluarMayoriaEdad = (edad,pais)=>
-    (edad>=18 && pais === "MX")?`En${pais} eres mayor de edad`:`En ${pais}NO eres mayor de edad`;
+const edadPersona=18;
+const mayoriaEdadMX=18;
+const mayoriaEdadUS=21;
+let evaluarMayoriaEdad=(edad)=>
+    edad>=18?"Eres mayor de edad.":"No eres mayor de edad."
 
-console.log("Evaluando la mayoria de edad de una persona en México ..")
-console.log(evaluarMayoriaEdad(edadPersona,"MX"));
+console.log("Evaluando la mayoría de edad de una persona: ");
+console.log(evaluarMayoriaEdad(edadPersona));
 
-console.log("Evaluando la mayoria de edad de una persona en Estados Unidos ..")
-console.log(evaluarMayoriaEdad(edadPersona,"US"));
+//Sin embargo tenemos que considerar que la mayoría de edad varia en cada país por cuestiones legales, por lo que debemos considerar un segundo parámetro en la evaluación.
 
-console.log("%c3.- SWITCH - CASE (Elección por valor definido) ", style_console);
-//Calculando tu generacion en base a tu edad 
-let anioNacimiento=2005;
+evaluarMayoriaEdad=(edad, pais)=>
+    (edad>=18 && pais=="MX")?`En ${pais} eres mayor de edad.`:`En ${pais} NO eres mayor de edad.`
 
-switch(true)
-{
-    case (anioNacimiento<1968): //Baby Boomers
-    return "Baby Boomers"
 
-    case(anioNacimiento>1968 && anioNacimiento<=1980):
-    return "Generacion X"
+console.log("Evaluando la mayoría de edad de una persona en Estados Unidos")
+console.log(evaluarMayoriaEdad(edadPersona, "USA"))
 
-    case(anioNacimiento>1981 && anioNacimiento<=1993):
-    return "Generacion Y"
+console.log("Evaluando la mayoría de edad de una persona en Estados Unidos")
+console.log(evaluarMayoriaEdad(edadPersona, "MX"))
 
-    case(anioNacimiento>1968 && anioNacimiento<=1980):
-    return "Generacion Z"
 
-    case(anioNacimiento>1994 && anioNacimiento<=2010):
-    return "CENTENIALS "
 
-    case(anioNacimiento>2010):
-    return "KRYSTAL"
-    
+console.log("%c3.- SWITCH - CASE (Elección por valor definido)", style_console);
 
+//Calculando tu generación en base a tu edad
+let anioNacimiento;
+let asignaGeneracion=(anioNacimiento)=>{
+
+    switch(true){
+        case(anioNacimiento<1968):
+        return "Baby Boomer";
+
+        case(anioNacimiento>1968 && anioNacimiento<=1980):
+        return "Generación X";
+
+        case(anioNacimiento>1980 && anioNacimiento<=1994):
+        return "Generación Y/Millennial";
+
+        case(anioNacimiento>1994 && anioNacimiento<=2009):
+        return "Generación Z/Centennial";
+
+        case(anioNacimiento>2009):
+        return "Genración Alfa";
+    }
 }
 
-console.log(`Dado que nacio en el año 1997 soy de la generacion: ${asinaGeneracion }`)
+console.log(`Dado que nací en el año 1999 soy de la generación ${asignaGeneracion(2003)}`)
 
 
+console.log("%c4.- Manejo de Escepciones (TRY/ CATCH)", style_console);
+
+
+//En algunos ocasiones existiran errores que no son culpa del programa, si no del usuario, la red, el so o incluso de un middleware, pero que si duda debemos controlar para evitar las fallas de ejecución.
+
+
+console.log("Initentamos dividir : 0/10, el resultado es: ")
+try{
+    let result= 0/10;
+    console.log(result)
+}
+catch(error)
+{
+    console.log("Ocurrio un error: "+error.message);
+}
+
+
+console.log("Initentamos dividir : 10/0, el resultado es: ")
+try{
+    let result= 10/0;
+    console.log(result)
+}
+catch(error)
+{
+    console.log("Ocurrio un error: "+error.message);
+}
+
+console.log("Initentamos dividir : a/0, el resultado es: ")
+try{ //intentamos
+    let result= "a"/0; //dividir una letra entre 0
+    console.log(result)
+}
+catch(error)
+{
+    console.log("Ocurrio un error: "+error.message);
+}
+
+
+console.log("Initentamos dividir : la variable a/0, el resultado es: ")
+try{ //intentamos
+    let result= a/0; //dividir una variable no definida 
+    console.log(result)
+}
+catch(error)
+{
+    console.log("Ocurrio un error: "+error.message);
+}
+
+console.log("Initentamos dividir : el valor de la variable x/ entre el valor de la variable y, el resultado es:")
+try{ //intentamos
+    let x=8, y=2, result=x/y; //dividir una variable no definida 
+    console.log(result)
+}
+catch(error)
+{
+    console.log("Ocurrio un error: "+error.message);
+}
+
+
+console.log("%c5. Control de Ciclos  (BREAK / CONTINUA) " ,style_console);
+
+//En algunas ocasiones sera importante detener un ciclo der manera abrupta para controlar casos especiales en un ciclo...
+
+console.log("Vamos a contarde 1 al 10...")
+for(let num =1;num<=10;num++){
+    console.log(num)
+}
+
+console.log("Ahorita necesitamos que si llegas al 7 pares de contar ... suponiendo que solo por hoy es de mala suerte ..")
+
+for( num =1;num<=10;num++)
+    if(num==7)
+        break;
+else
+    console.log(num);
+
+    console.log("Ahorita necesitamos que si llegas al 7 te saltes ese numero y continues")
+
+for( num =1;num<=10;num++)
+    if(num==7)
+        continue;
+else
+    console.log(num);
+ 
+
+    console.log("%c6.- Ciclo Iterativo - (FOR) ", style_console)
+
+    // Recorre de manera iterativo (i), de incremental o decremental 
+
+    console.log("Los dias de la semana son  de orden ascendente son :")
+let dias=["Domingo","Lunes","Martes","Miercoles","Jueves","Viernes","Sabado"]
+
+for(let i=0;i<dias.length-1;i++){
+console.log(dias[i])
+}
+
+console.log("Ahora vamos a imprimir con los meses en orden descendente")
+const meses= ["Enero","Febrero","Narzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"]
+for(let i=11;i>=0;i--){
+console.log(meses[i])
+}
+
+console.log("%c7.- Ciclo Condicionales - (WHILE)",style_console);
+//Estos ciclos (BUCKLE) dependen de una condicion para continuar ejecutandose 
+let finDeSemana = false;
+let mensaje = "";
+let j=0;
+while(j<7){
+
+
+switch(dias[j]){
+    case 0: 
+    finDeSemana = true;
+    mensaje = "Mormingoooooo..... ZzZzZzZzZZZZZzzzzzzz"
+    break;
+    case 1:
+        finDeSemana = false;
+        mensaje ="San Lunes a chambiar ..."
+        break;
+        case 2: 
+        finDeSemana = false;
+        mensaje ="Segundo dia de chamba a darle..."
+        break;
+        case 3: 
+        finDeSemana = false;
+        mensaje ="Ya meroooo el miercoles si se puede "
+        break;
+        case 4: 
+        finDeSemana = false
+        mensaje =" Ya jueveees que hueva vdd? pero si se puede "
+        break;
+        case 5: 
+        finDeSemana = false
+        mensaje = "Vierneeeeeees  si se pudo , recuerda eres lo que comes "
+        break;
+        case 6: 
+        finDeSemana = true;
+        mensaje ="Por fin , fin de semanaaaaaaa si se pudo "
+        break;
+}
+console.log(`Dia ${j} ${dias[j]}`)
+console.log(`Mensaje del dia : ${mensaje[j]}`)
+j++;
+}
+ 
